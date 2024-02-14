3d67d045e80f28abe467953ed730a03d12678328
MinimionBlack <64515867+MinimionBlack@users.noreply.github.com>
2024-02-14 12:50:36 -0500

    Adds Class Session 02-14-2024
    Class Session include
    - Messing Around with FPS Controllers
    - Working with Scripting
    - And Visual Scripting

---
+++ 02-terrain-MinimionBlack-main/Assets/MoveCapsule.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCapsule : MonoBehaviour
+{
+    
+    public Vector3 moveVector = new Vector3 (0, 0, 1);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(moveVector * Time.deltaTime);
+            Debug.Log(Time.deltaTime);
+        }
+    }
+
+}
